cdbeb25abf8b6eb0c2bd0b75d0843d2ec36b1c1d
Silas S. Brown <ssb22@cam.ac.uk>
2019-07-30 08:30:34 +0100

    update

--- git-sync.sh
+++ git-sync.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git pull --no-edit
-wget -N http://people.ds.cam.ac.uk/ssb22/gradint/wm65favs.py
-wget -N http://people.ds.cam.ac.uk/ssb22/gradint/timer-adjust.py
-wget -N http://people.ds.cam.ac.uk/ssb22/gradint/SBminutes.py
+wget -N http://people.ds.cam.ac.uk/ssb22/gradint/wm65favs.py http://people.ds.cam.ac.uk/ssb22/gradint/timer-adjust.py http://people.ds.cam.ac.uk/ssb22/gradint/SBminutes.py http://people.ds.cam.ac.uk/ssb22/gradint/wmSMSsend.py
+git add *.py
 git commit -am update && git push
---
+++ wmSMSsend.py
@@ -0,0 +1,23 @@
+# Python 2 code to send SMS message programatically on Windows Mobile (2003SE/5/6/6.5)
+# Silas S. Brown - public domain
+
+def send_SMS_message(number, unicode_text, delivery_confirmation=1):
+  print "Sending to "+number+"..."
+  handle = wintypes.DWORD()
+  ret = ctypes.cdll.sms.SmsOpen(u"Microsoft Text SMS Protocol",
+      wintypes.DWORD(2),ctypes.byref(handle),None)
+  if not (ret==0 and handle): raise Exception("SmsOpen error")
+  class SMSaddress(ctypes.Structure):
+    _fields_ = [("smsatAddressType",ctypes.c_int),
+      ("ptsAddress",ctypes.c_wchar * 256)]
+  unicode_text = u"" + unicode_text # make sure it's Unicode
+  if delivery_confirmation: byte1="\2"
+  else: byte1="\0"
+  ret = ctypes.cdll.sms.SmsSendMessage(handle,None,
+      ctypes.byref(SMSaddress(0,number)),None,unicode_text,
+      wintypes.DWORD(2*len(unicode_text)),byte1+"\0\0\0\3\0\0\0\0\0\0\0",
+      wintypes.DWORD(12),ctypes.c_int(0),wintypes.DWORD(0),None)
+  if not ret==0: raise Exception("SmsSendMessage error")
+  print "OK, closing"
+  ctypes.cdll.sms.SmsClose(handle)
+  print "closed"
