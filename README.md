c3cff392e85140824b5557ef5e9d3a7c1bc134d5
Silas S. Brown <ssb22@cam.ac.uk>
2019-10-29 07:46:42 +0000

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # wm6-utils
-Python utilities for old WM6 phones, from http://people.ds.cam.ac.uk/ssb22/gradint/wm61notes.html etc
+Python utilities for old WM6 phones, from http://ssb22.user.srcf.net/gradint/wm61notes.html and http://ssb22.user.srcf.net/gradint/s60.html etc
 
-(also mirrored at http://ssb22.gitlab.io/gradint/wm61notes.html as the Cambridge "DS-Web" server sometimes gets taken down for several days of maintenance)
+(the above two pages are also mirrored at http://ssb22.gitlab.io/gradint/wm61notes.html and http://ssb22.gitlab.io/gradint/s60.html just in case)
--- git-sync.sh
+++ git-sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull --no-edit
-wget -N http://people.ds.cam.ac.uk/ssb22/gradint/wm65favs.py http://people.ds.cam.ac.uk/ssb22/gradint/timer-adjust.py http://people.ds.cam.ac.uk/ssb22/gradint/SBminutes.py http://people.ds.cam.ac.uk/ssb22/gradint/wmSMSsend.py
+wget -N http://ssb22.user.srcf.net/gradint/wm65favs.py http://ssb22.user.srcf.net/gradint/timer-adjust.py http://ssb22.user.srcf.net/gradint/SBminutes.py http://ssb22.user.srcf.net/gradint/wmSMSsend.py http://ssb22.user.srcf.net/s60/pwi2txt.sh http://ssb22.user.srcf.net/s60/csc2vcf.py http://ssb22.user.srcf.net/s60/csm2txt.py
 git add *.py
 git commit -am update && git push
