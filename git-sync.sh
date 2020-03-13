3badab689fd5869b320acd95487e015f0324e51d
Silas S. Brown <ssb22@cam.ac.uk>
2020-03-13 00:06:23 +0000

    Remove git-sync.sh (updates now done by homepage/Makefile itself)

--- git-sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git pull --no-edit
-wget -N http://ssb22.user.srcf.net/gradint/wm65favs.py http://ssb22.user.srcf.net/gradint/timer-adjust.py http://ssb22.user.srcf.net/gradint/SBminutes.py http://ssb22.user.srcf.net/gradint/wmSMSsend.py http://ssb22.user.srcf.net/s60/pwi2txt.sh http://ssb22.user.srcf.net/s60/csc2vcf.py http://ssb22.user.srcf.net/s60/csm2txt.py
-git add *.py
-git commit -am update && git push
